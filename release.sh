06028fae6b8c2385d5efb21bfde9b1f98e55ec06
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2024-08-21 17:23:11 +0300

    Add release.sh build script

---
+++ release.sh
@@ -0,0 +1,23 @@
+#/bin/sh
+
+OUTDIR=t14s-packages
+
+if [[ $1 != "--skip-build" ]]; then
+    echo 'Building packages...'
+    for pkg in "linux-t14s" "t14s-firmware" "t14s-keyring" ; do
+        cd $pkg
+        PKGEXT=.pkg.tar.xz CARCH=aarch64 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- nice -19 makepkg -sCA
+        cd ..
+    done
+fi
+
+[[ -d $OUTDIR ]] && rm -rf $OUTDIR
+mkdir $OUTDIR && cd $OUTDIR
+
+cp ../*/*.pkg.tar.xz ./
+
+for pkg in *.pkg.tar.xz; do
+    gpg --detach-sign "${pkg}"
+done
+
+repo-add --sign linux-t14s.db.tar.gz *.pkg.tar.xz
